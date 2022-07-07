d01d38c58fc24073bd506ae79b919c3e9c2de1c4
AbdullahAyan <75751013+AbdullahAyan@users.noreply.github.com>
2022-07-07 03:29:36 +0300

    initial commit

---
+++ WeSplit/WeSplit/Preview Content/Preview Assets.xcassets/WeSplit/WeSplit/WeSplitApp.dataset/WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitApp.swift
+//  WeSplit
+//
+//  Created by Abdullah Ayan on 6.07.2022.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
