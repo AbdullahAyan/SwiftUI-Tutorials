d676aa46e0c39376698bfb6f9469f6f9dc03cf68
AbdullahAyan <75751013+AbdullahAyan@users.noreply.github.com>
2022-07-12 00:41:47 +0300

    trying xcode commit

--- ViewsAndModifiers/ViewsAndModifiers/ContentView.swift
+++ ViewsAndModifiers/ViewsAndModifiers/ContentView.swift
@@ -10,26 +10,54 @@ import SwiftUI
 struct ContentView: View {
     let motto1 = Text("Draco dormiens")
     let motto2 = Text("nunquam titillandus")
-
+    
     var body: some View {
-        NavigationView {
-            motto1
-            motto2
-            spells
+        GridStack(rows: 4, columns: 4) { row, col in
+            Image(systemName: "\(row * 4 + col).circle")
+            Text("R\(row) C\(col)")
         }
-        .foregroundStyle(.ultraThinMaterial)
-
+        
         
     }
     
     @ViewBuilder var spells: some View {
-            Text("Lumos")
-            Text("Obliviate")
+        Text("Lumos")
+        Text("Obliviate")
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+    }
+}
+
+
+struct Title: ViewModifier {
+    func body(content: Content) -> some View {
+        content
+            .font(.largeTitle)
+            .foregroundColor(.white)
+            .padding()
+            .background(.blue)
+            .clipShape(RoundedRectangle(cornerRadius: 10))
+    }
+}
+
+struct GridStack<Content: View>: View {
+    let rows: Int
+    let columns: Int
+    @ViewBuilder let content: (Int, Int) -> Content
+    
+    var body: some View {
+        VStack {
+            ForEach(0..<rows, id: \.self) { row in
+                HStack {
+                    ForEach(0..<columns, id: \.self) { column in
+                        content(row, column)
+                    }
+                }
+            }
+        }
     }
 }
