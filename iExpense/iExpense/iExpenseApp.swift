009f3d370f79dcdbce64d907e8979fc02fc789d4
AbdullahAyan <75751013+AbdullahAyan@users.noreply.github.com>
2022-08-04 18:34:11 +0300

    initial iExpense

---
+++ iExpense/iExpense/iExpenseApp.swift
@@ -0,0 +1,17 @@
+//
+//  iExpenseApp.swift
+//  iExpense
+//
+//  Created by Abdullah Ayan on 4.08.2022.
+//
+
+import SwiftUI
+
+@main
+struct iExpenseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
