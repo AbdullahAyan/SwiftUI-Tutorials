b286bcec629d06204d76dcfc1c95b9be11e65f6d
AbdullahAyan <75751013+AbdullahAyan@users.noreply.github.com>
2022-08-11 00:20:40 +0300

    preworks

--- Moonshot/Moonshot/ContentView.swift
+++ Moonshot/Moonshot/ContentView.swift
@@ -8,11 +8,21 @@
 import SwiftUI
 
 struct ContentView: View {
+    let layout = [
+        GridItem(.adaptive(minimum: 80, maximum: 120)),
+    ]
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        ScrollView(.vertical) {
+            LazyHGrid(rows: layout) {
+                ForEach(0..<1000) {
+                    Text("Item \($0)")
+                }
+            }
+        }
     }
 }
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
