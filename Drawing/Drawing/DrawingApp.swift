b05c22fa5feeff134fea540cc99311851c335711
AbdullahAyan <75751013+AbdullahAyan@users.noreply.github.com>
2022-08-25 23:24:28 +0300

    Drawing

---
+++ Drawing/Drawing/DrawingApp.swift
@@ -0,0 +1,17 @@
+//
+//  DrawingApp.swift
+//  Drawing
+//
+//  Created by Abdullah Ayan on 24.08.2022.
+//
+
+import SwiftUI
+
+@main
+struct DrawingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
