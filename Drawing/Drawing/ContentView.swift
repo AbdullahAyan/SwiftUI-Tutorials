4d58c0ea52b84e5cf62b726220ac2c8f27d2ac41
AbdullahAyan <75751013+AbdullahAyan@users.noreply.github.com>
2022-08-27 22:48:50 +0300

    Drwing updates

--- Drawing/Drawing/ContentView.swift
+++ Drawing/Drawing/ContentView.swift
@@ -9,21 +9,18 @@ import SwiftUI
 
 
 struct ContentView: View {
-    @State private var petalOffset = -20.0
-    @State private var petalWidth = 100.0
-
+    @State private var colorCycle = 0.0
+    
     var body: some View {
         VStack {
-            Flower(petalOffset: petalOffset, petalWidth: petalWidth)
-                .fill(.red, style: FillStyle(eoFill: true))
-            
-            Text("Offset")
-            Slider(value: $petalOffset, in: -40...40)
-                .padding([.horizontal, .bottom])
-
-            Text("Width")
-            Slider(value: $petalWidth, in: 0...100)
-                .padding(.horizontal)
+            ZStack {
+                ColorCyclingCircle(amount: colorCycle)
+                    .frame(width: 300, height: 300)
+                
+                
+            }
+            .drawingGroup()
+            Slider(value: $colorCycle)
         }
     }
 }
@@ -32,30 +29,66 @@ struct Triangle: Shape {
     func path(in rect: CGRect) -> Path {
         var path = Path()
         
-        path.move(to: CGPoint(x: rect.midX, y: rect.minY))
+        path.move(to: CGPoint(x: rect.minX, y: rect.minY))
+        path.addLine(to: CGPoint(x: rect.maxX, y: rect.midY))
         path.addLine(to: CGPoint(x: rect.minX, y: rect.maxY))
-        path.addLine(to: CGPoint(x: rect.maxX, y: rect.maxY))
-        path.addLine(to: CGPoint(x: rect.midX, y: rect.minY))
-        
+        path.addLine(to: CGPoint(x: rect.minX, y: rect.minY))
         return path
+    }
+    
+    
+    
+}
+
+struct ColorCyclingCircle: View {
+    var amount = 0.0
+    var steps = 100
+    
+    var body: some View {
+        ZStack {
+            ForEach(0..<steps) { value in
+                Circle()
+                    .inset(by: Double(value))
+                    .strokeBorder(
+                        LinearGradient(
+                            gradient: Gradient(colors: [
+                                color(for: value, brightness: 1),
+                                color(for: value, brightness: 0.5)
+                            ]),
+                            startPoint: .top,
+                            endPoint: .bottom
+                        ),
+                        lineWidth: 2
+                    )
+            }
+        }
+    }
+    
+    func color(for value: Int, brightness: Double) -> Color {
+        var targetHue = Double(value) / Double(steps) + amount
+        
+        while targetHue > 1 {
+            targetHue -= 1
+        }
+        
+        return Color(hue: targetHue, saturation: 1, brightness: brightness)
     }
 }
 
 struct Arc: InsettableShape {
+    
+    var rotationAdjustment: Angle = .degrees(90)
     var startAngle: Angle
     var endAngle: Angle
-    var clockwise: Bool
+    var clockWise: Bool
     var insetAmount = 0.0
     
     func path(in rect: CGRect) -> Path {
-        let rotationAdjustment = Angle.degrees(90)
-        let modifiedStart = startAngle - rotationAdjustment
-        let modifiedEnd = endAngle - rotationAdjustment
-        
         var path = Path()
-        path.addArc(center: CGPoint(x: rect.midX, y: rect.midY), radius: rect.width / 2 - insetAmount, startAngle: modifiedStart, endAngle: modifiedEnd, clockwise: !clockwise)
         
+        path.addArc(center: CGPoint(x: rect.midX, y: rect.midY), radius: (rect.width / 2) - insetAmount, startAngle: startAngle - rotationAdjustment, endAngle: endAngle - rotationAdjustment, clockwise: !clockWise)
         
+        path.addArc(center: CGPoint(x: rect.midX, y: rect.midY), radius: (rect.width / 2) - insetAmount, startAngle: startAngle - rotationAdjustment, endAngle: endAngle - rotationAdjustment, clockwise: clockWise)
         return path
     }
     
@@ -64,40 +97,36 @@ struct Arc: InsettableShape {
         arc.insetAmount += amount
         return arc
     }
+    
+    
 }
 
 struct Flower: Shape {
-    // How much to move this petal away from the center
-    var petalOffset: Double = -20
     
-    // How wide to make each petal
+    var petalOffset: Double = -20
     var petalWidth: Double = 100
     
     func path(in rect: CGRect) -> Path {
-        // The path that will hold all petals
+        
         var path = Path()
         
-        // Count from 0 up to pi * 2, moving up pi / 8 each time
-        for number in stride(from: 0, to: Double.pi * 2, by: Double.pi / 8) {
-            // rotate the petal by the current value of our loop
+        for number in stride(from: 0, to: Double.pi * 2, by: Double.pi / 4) {
+            
             let rotation = CGAffineTransform(rotationAngle: number)
             
-            // move the petal to be at the center of our view
             let position = rotation.concatenating(CGAffineTransform(translationX: rect.width / 2, y: rect.height / 2))
             
-            // create a path for this petal using our properties plus a fixed Y and height
-            let originalPetal = Path(ellipseIn: CGRect(x: petalOffset, y: 0, width: petalWidth, height: rect.width / 2))
+            let petal = Path(ellipseIn: CGRect(x: petalOffset, y: 0, width: petalWidth, height: rect.width / 2))
             
-            // apply our rotation/position transformation to the petal
-            let rotatedPetal = originalPetal.applying(position)
+            let rotatedPetal = petal.applying(position)
             
-            // add it to our main path
             path.addPath(rotatedPetal)
+            
         }
-        
-        // now send the main path back
         return path
     }
+    
+    
 }
 
 struct ContentView_Previews: PreviewProvider {
